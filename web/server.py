c7ee3731b7797c1e79774f71eba62c6328d35b0b
bashkirtsevich <bashkirtsevich@gmail.com>
2017-04-12 18:56:56 +0500

    Add HTTP-500 error page

--- web/server.py
+++ web/server.py
@@ -46,7 +46,11 @@ def start_server(mongodb_uri, host, port):
 
         @app.errorhandler(404)
         def page_not_found(e):
-            return render_template('404.html'), 404
+            return render_template("error.html", error_code=404), 404
+
+        @app.errorhandler(500)
+        def internal_server_error(e):
+            return render_template("error.html", error_code=500), 500
 
         @app.route("/")
         def show_index():
