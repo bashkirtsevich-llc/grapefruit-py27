e217dbef0d1026c967e0900922f7176b523dc759
bashkirtsevich <bashkirtsevich@gmail.com>
2017-04-23 18:25:31 +0500

    Pass try_load_metadata arguments as dictionary

--- service/indexer.py
+++ service/indexer.py
@@ -28,9 +28,12 @@ def __index_next_info_hash(db, try_load_metadata):
         ))
 
         if torrents:
-            try_load_metadata(choice(torrents)["info_hash"],
-                              lambda metadata: __store_metadata(db, metadata, try_load_metadata),
-                              lambda: __index_next_info_hash(db, try_load_metadata))
+            args = {
+                "info_hash": choice(torrents)["info_hash"],
+                "on_torrent_loaded": lambda metadata: __store_metadata(db, metadata, try_load_metadata),
+                "on_no_peers_found": lambda: __index_next_info_hash(db, try_load_metadata)
+            }
+            try_load_metadata(**args)
 
             break
 
