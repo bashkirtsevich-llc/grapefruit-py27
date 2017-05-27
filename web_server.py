32ed09ff71156b910b20328e70b80e549389abd8
bashkirtsevich <bashkirtsevich@gmail.com>
2017-05-27 11:05:50 +0500

    Write webserver logs into /dev/null

--- web_server.py
+++ web_server.py
@@ -1,7 +1,12 @@
+import logging
+import os
 from config import *
 from web.server import start_server
 
 if __name__ == '__main__':
+    logging.basicConfig(filename=os.devnull,
+                        level=logging.DEBUG)
+
     start_server(mongodb_uri=MONGODB_URI,
                  host=WEB_SERVER_HOST,
                  port=WEB_SERVER_PORT,
