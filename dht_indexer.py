a297ecfc713ba54058600e1e50146a73ab095fef
bashkirtsevich <bashkirtsevich@gmail.com>
2017-05-31 01:47:44 +0500

    Implement start_indexers for multiprocessing

--- dht_indexer.py
+++ dht_indexer.py
@@ -1,8 +1,26 @@
+from multiprocessing import Process
 from dht_indexer_config import *
 from service.indexer import start_indexer
 
+def start_indexers(web_server_api_url, indexers_info):
+    processes = []
+
+    for indexer_info in indexers_info:
+        args = {
+            "web_server_api_url": web_server_api_url,
+            "port": indexer_info["port"],
+            "node_id": indexer_info["node_id"],
+            "bootstrap_node_address": indexer_info["bootstrap"]
+        }
+
+        p = Process(target=start_indexer, kwargs=args)
+        p.start()  # Startup new process
+
+        processes.append(p)
+
+    # Wait until all processes end
+    map(lambda process: process.join(), processes)
+
 if __name__ == '__main__':
-    start_indexer(web_server_api_url=WEB_SERVER_API_URL,
-                  port=DHT_INDEXER_PORT,
-                  node_id=DHT_INDEXER_NODE_ID,
-                  bootstrap_node_address=DHT_INDEXER_BOOTSTRAP_ADDRESS)
+    start_indexers(web_server_api_url=WEB_SERVER_API_URL,
+                   indexers_info=DHT_INDEXERS_INFO)
--- dht_indexer_config.py
+++ dht_indexer_config.py
@@ -1,6 +1,9 @@
 # Database address
 WEB_SERVER_API_URL = "http://127.0.0.1:8081/api"
 
-DHT_INDEXER_PORT = 6881
-DHT_INDEXER_NODE_ID = None
-DHT_INDEXER_BOOTSTRAP_ADDRESS = ("router.bittorrent.com", 6881)
+DHT_DEFAULT_BOOTSTRAP_ADDRESS = ("router.bittorrent.com", 6881)
+
+DHT_INDEXERS_INFO = map(lambda port: {"port": port,
+                                      "node_id": None,
+                                      "bootstrap": DHT_DEFAULT_BOOTSTRAP_ADDRESS},
+                        xrange(6881, 6882))
