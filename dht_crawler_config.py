af37b01a7077c124284cb035156afecec574db0e
bashkirtsevich <bashkirtsevich@gmail.com>
2017-05-31 16:43:24 +0500

    Little code formatting

--- dht_crawler_config.py
+++ dht_crawler_config.py
@@ -1,4 +1,5 @@
 # Database address
 WEB_SERVER_API_URL = "http://127.0.0.1:8081/api"
 
-DHT_CRAWLER_NODES_INFO = map(lambda port: {"port": port, "node_id": None}, xrange(6882, 6902, 2))
+DHT_CRAWLER_NODES_INFO = map(lambda port: {"port": port, "node_id": None},
+                             xrange(6882, 6902, 2))
