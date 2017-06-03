753e88400ef55ed604cbd97a540344324652562e
bashkirtsevich <bashkirtsevich@gmail.com>
2017-06-04 00:11:10 +0500

    Revert "Increase delay time (30s) before startup crawler and indexer"
    This reverts commit b36f98cb0b3ea12f44b2006671e4779a238d3286.

--- start_grapefruit.sh
+++ start_grapefruit.sh
@@ -1,7 +1,7 @@
 echo "start web server"
 python web_server.py &
-echo "wait 30 seconds"
-sleep 30
+echo "wait 5 seconds"
+sleep 5
 echo "start crawler"
 python dht_crawler.py &
 echo "start indexer"
